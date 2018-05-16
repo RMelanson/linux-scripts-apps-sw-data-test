d6f190c57859909bc2870298d96f70842dcb417a
Robin Melanson <remcogold@gmail.com>
2018-05-15 22:12:50 -0400

    Update swDataTestBootstrap.sh

--- swDataTestBootstrap.sh
+++ swDataTestBootstrap.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-bs_currDir=$PWD
-#------------------- INITIAL BASIC TOOLS INSTALL --------------------
-yum update -y
-
-#------------------- INSTALL GITHUB --------------------
-yum install git -y
-
-#------------------- INSTALL MASTER BOOTSTRAP APPLICATIONS --------------------
-git clone https://github.com/RMelanson/linux-scripts-bootstraps.git $bootStrapsDir
-echo bootstraps installed in directory $bootStrapDir
-cd $bootStrapsDir
-. ./setup.sh
-
-cd $bs_currDir
+sw-test-data-currDir=$PWD
+swDir=/tmp/scripts/api/rest/stockWidgets
+alias cdsw='cd $swDir'
+git clone https://github.com/RMelanson/linux-scripts-apps-sw-test-data $swDir
+cd $swDir
+./setup.sh
+cd $sw-test-data-currDir
