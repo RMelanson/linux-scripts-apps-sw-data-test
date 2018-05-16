aad25ca9c2fbda8f776f450fca42c2f4c314ce5b
Robin Melanson <remcogold@gmail.com>
2018-05-15 22:26:24 -0400

    Update swDataTestBootstrap.sh

--- swDataTestBootstrap.sh
+++ swDataTestBootstrap.sh
@@ -3,6 +3,6 @@ sw-test-data-currDir=$PWD
 swDir=/tmp/scripts/api/rest/stockWidgets
 alias cdsw='cd $swDir'
 git clone https://github.com/RMelanson/linux-scripts-apps-sw-test-data $swDir
-. ./setup.sh
 cd $swDir
+. ./setup.sh
 cd $sw-test-data-currDir
