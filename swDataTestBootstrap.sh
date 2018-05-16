77681d2e04cd4a912040b0234b0fc14501af6525
Robin Melanson <remcogold@gmail.com>
2018-05-15 21:55:12 -0400

    Update swDataTestBootstrap.sh

--- swDataTestBootstrap.sh
+++ swDataTestBootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-currDir=$PWD
+bs_currDir=$PWD
+#------------------- INITIAL BASIC TOOLS INSTALL --------------------
+yum update -y
+
+#------------------- INSTALL GITHUB --------------------
+yum install git -y
+
 #------------------- INSTALL MASTER BOOTSTRAP APPLICATIONS --------------------
 git clone https://github.com/RMelanson/linux-scripts-bootstraps.git $bootStrapsDir
 echo bootstraps installed in directory $bootStrapDir
 cd $bootStrapsDir
 . ./setup.sh
 
-cd $currDir
+cd $bs_currDir
