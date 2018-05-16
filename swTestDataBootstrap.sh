0bfd9c7cd330aae4a461b1332aeb7ccd1ee2513b
Robin Melanson <remcogold@gmail.com>
2018-05-15 23:45:12 -0400

    Update swTestDataBootstrap.sh

--- swTestDataBootstrap.sh
+++ swTestDataBootstrap.sh
@@ -2,6 +2,6 @@
 swTestDataCurrDir=$PWD
 swDir=/tmp/scripts/apps/SW/test/data
 git clone https://github.com/RMelanson/linux-scripts-apps-sw-test-data.git $swDir
-. ./setup.sh
 cd $swDir
+. ./setup.sh
 cd $swTestDataCurrDir
