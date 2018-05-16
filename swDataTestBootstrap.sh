a30f5fadff9d0764756ac9bf4f8372a75d8a5dc7
Robin Melanson <remcogold@gmail.com>
2018-05-15 23:29:49 -0400

    Delete swDataTestBootstrap.sh

--- swDataTestBootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sw-test-data-currDir=$PWD
-swDir=/tmp/scripts/apps/SW/test
-alias cdsw='cd $swDir'
-git clone https://github.com/RMelanson/linux-scripts-apps-sw-test-data $swDir
-cd $swDir
-. ./setup.sh
-cd $sw-test-data-currDir
