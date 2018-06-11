3903520ab6b80504f95bbad189fae628172eb465
Robin Melanson <remcogold@gmail.com>
2018-06-11 00:36:17 -0400

    Update installTestData.sh

--- installTestData.sh
+++ installTestData.sh
@@ -1,6 +1,4 @@
 dataInstallDir=$PWD
-#SW_HOME=/home/SW_HOME
-#SW_TEST_DATA=$SW_HOME/input/test/swdata
 rm -rf $SW_TEST_DATA 2>&1 > /dev/null
 mkdir -p $SW_TEST_DATA
 cp -rf input/swdata/* $SW_TEST_DATA
