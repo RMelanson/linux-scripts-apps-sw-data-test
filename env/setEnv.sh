8e73306973fccdc682da7697c868ebf4b8ba9e68
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:01:03 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -2,5 +2,7 @@
 
 # SW TEST DATA CONFIGURATION
 pkg=swTestData
+gitRepo="linux-scripts-apps-sw-test-data.git"
+installDir="/tmp/scripts/apps/SW/test/data"
 export SW_TEST_DATA=$SW_HOME/input/swdata
 export SW_PROD_DATA=$SW_HOME/input/swdata
