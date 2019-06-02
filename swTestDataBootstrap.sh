a4b3e6fab662d2e3b945d40aba989560749ea8c3
Robin Melanson <remcogold@gmail.com>
2019-06-01 21:47:33 -0400

    Update swTestDataBootstrap.sh

--- swTestDataBootstrap.sh
+++ swTestDataBootstrap.sh
@@ -46,6 +46,7 @@ cd $installDir
 find . -name "*.sh" -exec chmod 700 {} \;
 
 # Setup Project
-./setup.sh 2>&1| tee setup.log
+echo "EXECUTING: ./setup.sh $*2>&1| tee setup.log"
+./setup.sh $*2>&1| tee setup.log
 
 cd $bootstrapDir
